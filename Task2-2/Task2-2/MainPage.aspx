df9becf06c45dbdab54d70a4c4dead1fa576ba2a
Mira <szewczyk.mira@gmail.com>
2015-10-28 21:40:26 +0100

    Zadanie 2.2 skonczone

---
+++ Task2-2/Task2-2/MainPage.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainPage.aspx.cs" Inherits="Task2_2.MainPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Zadanie 2.2</title>
+</head>
+<body>
+    <form runat="server">
+        <asp:CheckBox ID="check" runat="server" /> Check it if you want<br />
+        <asp:Button Text="Save a cookie" runat="server" id="save" OnClick="SaveCookie" /><br />
+        <asp:Button Text="Read a cookie" runat="server" ID="read" OnClick="ReadCookie" /><br />
+        <asp:Button Text="Delete cookie!" runat="server" ID="delete" OnClick="DeleteCookies"/>
+    </form>
+</body>
+</html>
---
+++ Task2-2/Task2-2/MainPage.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Task2_2
+{
+    public partial class MainPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void SaveCookie(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("CheckboxValue");
+            cookie.Value = check.Checked.ToString();
+            cookie.Expires = DateTime.Now.AddMinutes(10d);
+            Response.Cookies.Add(cookie);
+        }
+        protected void ReadCookie(object sender, EventArgs e)
+        {
+            HttpCookie cookie = Request.Cookies.Get("CheckboxValue");
+            if(cookie == null)
+            {
+                Response.Write("Cookie does not exist!");
+            } else
+            {
+                Response.Write(cookie.Value);
+            }
+        }
+        protected void DeleteCookies(object sender, EventArgs e)
+        {
+            HttpCookie cookie = Request.Cookies.Get("CheckboxValue");
+            cookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(cookie);
+        }
+    }
+}
