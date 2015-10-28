8e93f83e7dfd192c55d1b0b795a654831198827e
Mira <szewczyk.mira@gmail.com>
2015-10-28 20:33:14 +0100

    Get with button added

---
+++ Task2-1/Task2-1/MainPage.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainPage.aspx.cs" Inherits="Task2_1.MainPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        What you are looking for?
+        <input type="text" id="input" runat="server" />
+        <asp:Button OnClick="sendGet" runat="server" Text="Search"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Task2-1/Task2-1/MainPage.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+
+namespace Task2_1
+{
+    public partial class MainPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write(Request.QueryString["param"]);
+        }
+        protected void sendGet(object sender, EventArgs e)
+        {
+            Response.Redirect("MainPage.aspx?param=" + input.Value);
+        }
+    }
+}
