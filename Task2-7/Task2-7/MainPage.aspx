b1afb5a08ae30fc14ceaa1257d5b0744911a8018
Mira <szewczyk.mira@gmail.com>
2015-10-31 23:22:02 +0100

    Zadanie 2.7

---
+++ Task2-7/Task2-7/MainPage.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainPage.aspx.cs" Inherits="Task2_7.MainPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <input type="file" id="Plik" runat="server" />
+        <asp:Button runat="server" OnClick="HandleFile" Text="Submit" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Task2-7/Task2-7/MainPage.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace Task2_7
+{
+    public partial class MainPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void HandleFile(object sender, EventArgs e)
+        {
+            Response.Clear();
+            Response.ContentType = "text/xml";
+            Response.AppendHeader("Content-Disposition", "attachment;filename=FileName.xml");
+            XmlTextWriter xWriter = new XmlTextWriter(Response.OutputStream, System.Text.Encoding.UTF8);
+            xWriter.Formatting = Formatting.Indented;
+            xWriter.WriteStartDocument();
+            //Create Parent element
+            xWriter.WriteStartElement("opis");
+            //Create Child elements
+            xWriter.WriteElementString("nazwa", Plik.PostedFile.FileName);
+            xWriter.WriteElementString("rozmiar", Plik.PostedFile.ContentLength.ToString());
+            xWriter.WriteEndElement();
+
+            //End writing top element and XML document
+            xWriter.WriteEndDocument();
+            xWriter.Close();
+            Response.End();
+        }
+    }
+}
