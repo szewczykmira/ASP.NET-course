7c209bab551a96ed7bdf20cff83362f587adcc30
Mira <szewczyk.mira@gmail.com>
2015-10-31 21:46:06 +0100

    Zadanie 2.3 - czytanie request.header i dodawanie do response.header

---
+++ Task2-3/Task2-3/MainPage.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainPage.aspx.cs" Inherits="Task2_3.MainPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <div id="dd" runat="server">
+            Request Header:
+    </div>
+    <form runat="server">
+        <asp:Button ID="set" runat="server" OnClick="AddHeader" Text="Dodaj do nagłówka" UseSubmitBehavior="false"/>
+        <asp:Button ID="get" runat="server" OnClick="ReadHeader" Text="Przeczytaj nagłówek" UseSubmitBehavior="false" />  
+    </form>
+</body>
+</html>
---
+++ Task2-3/Task2-3/MainPage.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Task2_3
+{
+    public partial class MainPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        protected void AddHeader(object sender, EventArgs e)
+        {
+            Response.AddHeader("value", "42");
+        }
+        protected void ReadHeader(object sender, EventArgs e)
+        {
+            foreach (var key in Request.Headers.AllKeys)
+            {
+                dd.InnerText += key + "=" + Request.Headers[key] + Environment.NewLine;
+            }
+        }
+    }
+}
