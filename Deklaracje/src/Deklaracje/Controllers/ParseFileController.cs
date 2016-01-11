108c3b40f9b76c6c42bc4da439b1543f36ddb1a9
Mira <szewczyk.mira@gmail.com>
2016-01-11 22:05:11 +0100

    Second task almost finished

---
+++ Deklaracje/src/Deklaracje/Controllers/ParseFileController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Http;
+using System.Net.Http.Headers;
+
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Deklaracje.Controllers
+{
+    public class ParseFileController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Display()
+        {
+            IFormFile UFile = Request.Form.Files.GetFile("UploadedFile");
+            ViewData["UFileName"] = ContentDispositionHeaderValue.Parse(UFile.ContentDisposition).FileName.Trim('"');
+            //long FileSize = ContentDispositionHeaderValue.Parse(UFile.ContentDisposition).Size.Value;
+            //ViewData["UFileSize"] = FileSize.ToString();
+            //ViewData["UFileMode"] = (FileSize % 0xFFFF).ToString();
+            return View();
+        }
+    }
+}
